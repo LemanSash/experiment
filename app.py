dda9b664c8ab61d06d4fc3645a6205f3682af538
LemanSash <leman.sash@ya.ru>
2026-01-19 20:46:27 +0300

    pumps

--- app.py
+++ app.py
@@ -1330,7 +1330,7 @@ def save_bart():
     user_id = session['user_id']
 
     trial_number = data['trialNumber']
-    pump_number = data['pumpNumber']
+    pump_number = data['pumps']
     break_point = data['breakPoint']
     reaction_time = data['reaction_time']
 
