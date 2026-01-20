8828020163dc1537788f8665dfbac3f659e5fea1
LemanSash <leman.sash@ya.ru>
2026-01-20 21:09:09 +0300

    smtp

--- app.py
+++ app.py
@@ -272,7 +272,7 @@ def before_request():
 
     # Разрешить пропуска проверок для маршрутов авторизации и регистрации
     allowed_routes = ['login', 'register', 'logout', 'home', 'terms', 'agreement', 'reset_password_request',
-                      'reset_password/<token>']
+                      'reset_password']
 
     # Если текущий запрос не входит в разрешённые маршруты и пользователь не залогинен
     if request.endpoint not in allowed_routes and 'user_id' not in session:
