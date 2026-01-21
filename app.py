f8a8cca679d9ed8bc5d11c262df07f6dc3de5b25
LemanSash <leman.sash@ya.ru>
2026-01-21 21:56:45 +0300

    bart

--- app.py
+++ app.py
@@ -80,8 +80,8 @@ def init_db():
 
     # cursor.execute('DROP TABLE IF EXISTS questionnaire_responses CASCADE')
     # cursor.execute('DROP TABLE IF EXISTS users CASCADE')
-    # cursor.execute('TRUNCATE TABLE bart_results RESTART IDENTITY CASCADE')
-    # cursor.execute('TRUNCATE TABLE user_progress RESTART IDENTITY CASCADE')
+    cursor.execute('TRUNCATE TABLE bart_results RESTART IDENTITY CASCADE')
+    cursor.execute('TRUNCATE TABLE user_progress RESTART IDENTITY CASCADE')
     #Create users table
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS users (
@@ -1522,7 +1522,7 @@ def save_bart():
 
     return jsonify({
         'status': 'ok', 
-        'redirect_url': url_for('intermediate', task_name='igt') if is_final_trial else None})
+        'redirect_url': url_for('intermediate', task_name='bart') if is_final_trial else None})
 
 
 @app.route('/intermediate/<task_name>')
