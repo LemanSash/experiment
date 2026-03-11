e652fef0ba6549c8a8a5b77fad9dc360b5e7a225
LemanSash <leman.sash@ya.ru>
2026-03-11 15:52:46 +0300

    limiter

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app = Flask(__name__)
 limiter = Limiter(
     get_remote_address,
     app=app,
-    default_limits=["2000 per day", "1000 per hour", "120 per minute"],
+    default_limits=["5000 per day", "3000 per hour", "1000 per minute"],
     storage_uri="memory://",
 )
 
